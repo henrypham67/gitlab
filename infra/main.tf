62b6074be5a1bac139ef1d0866da92ad90538111
Hieu Pham <minhhieu060799@gmail.com>
2025-10-02 22:27:00 +0700

    [Update] replace minio with s3

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,44 @@ module "cluster" {
   max_nodes     = 9
 }
 
+locals {
+  apps_need_storage = {
+    "gitlab" : [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:GetObjectAttributes",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload"
+    ],
+  }
+}
+
+module "storages" {
+  for_each = local.apps_need_storage
+  source   = "git::https://github.com/henrypham67/istio.git//modules/s3-pod-identity?ref=main"
+
+  # Core configuration
+  cluster_name         = module.cluster.cluster_name
+  application_name     = each.key
+  bucket_name          = "my-${each.key}-bucket-199907060500"
+  namespace            = "gitlab"
+  service_account_name = "${each.key}-sa"
+
+  # S3 configuration
+  enable_versioning = true
+  sse_algorithm     = "AES256"
+
+  s3_permissions = each.value
+
+  tags = {
+    Application = each.key
+    Environment = "production"
+    DataType    = "git"
+  }
+}
+
 resource "helm_release" "argocd" {
   chart            = "argo-cd"
   name             = "argo-cd"
